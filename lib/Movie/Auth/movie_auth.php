e37029b610b79972d012227e92c3b4cdfba28398
dptesting <debs_1_2_1@hotmail.co.uk>
2017-05-20 22:09:59 +0100

    added comments functionality, where only members can comment.  added validation to blogs and comments.  added different options for admin and non admin.

--- lib/Movie/Auth/movie_auth.php
+++ lib/Movie/Auth/movie_auth.php
@@ -7,26 +7,37 @@ use function Movie\Db\read_user;
 session_start(); //session_start() must always be called to ensure $_SESSION is populated properly, and if not, to issue a cookie to a browser so that it can be.
 
 function login($pdo, $username, $password) {
+
+
+    $username = stripcslashes($username);
+    $password = stripcslashes($password);
+
+
     $user = read_user($pdo, $username, $password);
+    password_verify($password, $user['password']);
+
+
 
     if ($username && password_verify($password, $user['password'])) {
-        $_SESSION['username'] = $username;
+        $_SESSION['login_user'] = $username; // Initializing Session 
+       // echo $username;
+      //  echo '<a href="index.php?logout"><span>Logout</span></a></li>';
         header('Location: index_1.php');
     } else {
         echo "Username or Password is invalid";
-        ;
     }
-}
 
-function logout() {
-    ob_end_clean();
-    session_destroy();
-    echo "logged out";
-    exit;
-}
-
-function require_login() {
-    if (empty($_SESSION['username'])) {
-        header('Location: login.php');
+    function logout() {
+        ob_end_clean();
+        session_destroy();
+        echo "logged out";
+        exit;
     }
+
+    function require_login() {
+        if (empty($_SESSION['username'])) {
+            header('Location: login.php');
+        }
+    }
+
 }
