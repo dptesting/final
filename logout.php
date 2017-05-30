cbf366522005e10c306035e6fb523ad7beeeee89
dptesting <debs_1_2_1@hotmail.co.uk>
2017-05-30 19:30:35 +0100

    updated logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,4 @@
 <?php
-
-include 'lib/Movie/View/movie_view.php';
-
-use function Movie\View\display;
-
-echo display('header');
-
 session_start();
 session_destroy();
 header('Location: index.php');
